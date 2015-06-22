a3439b0e41d0a7c9b4e0aecf667ac8ae790efefe
dan <dan@mainframe>
2015-06-22 08:39:39 +0100

    changed returnable into an interface!

--- src/com/onedanstudio/messingwithjava/Returnable.java
+++ src/com/onedanstudio/messingwithjava/Returnable.java
@@ -3,11 +3,6 @@ package com.onedanstudio.messingwithjava;
 /**
  * Created by dan on 22/06/15.
  */
-public abstract class Returnable implements Runnable {
-    @Override
-    public void run() {
-
-    }
-
-    abstract public Object ret();
+interface Returnable extends Runnable {
+    Object ret();
 }
